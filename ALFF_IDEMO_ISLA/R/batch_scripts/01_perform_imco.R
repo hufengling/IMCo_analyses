5387bf19439474a4b01a01c89a38eddbbd60eeef
hufengling <hufengling@gmail.com>
2021-09-14 12:20:34 -0400

    working on coupling bsub

--- ALFF_IDEMO_ISLA/R/batch_scripts/01_perform_imco.R
+++ ALFF_IDEMO_ISLA/R/batch_scripts/01_perform_imco.R
@@ -1,19 +1,46 @@
 library(IMCo)
 library(stringr)
+library(magrittr)
 
 args <- commandArgs(trailingOnly = TRUE)
-files <- args[1]
+files <- args[1] %>% 
+        str_split(pattern = ",") %>% 
+        unlist() %>% 
+        as.list()
+mask <- args[2]
+output_dir <- args[3]
+out_name <- args[4]
+fwhm <- args[5]
+propMiss <- args[6]
 
-out_dir <- args[2]
+change_mode <- function(mode) {
+        if (mode == "global_wcov") {
+                path <- "global_wcov"
+                pca_type <- "global"
+                matrix_type <- "wcov"
+        } else if (mode == "unscaled_wcor") {
+                path <- "unscaled_wcor"
+                pca_type <- "unscaled"
+                matrix_type <- "wcor"
+        } else
+                stop("Must enter global_wcov or unscaled_wcor")
+        
+        mode_output_dir <- file.path(output_dir, "niftis/coupled", path)
+        dir.create(mode_output_dir, showWarnings = FALSE, recursive = TRUE)
+        
+        imco(files, 
+             mask,
+             out_dir = mode_output_dir,
+             out_name = out_name,
+             fwhm = 3,
+             propMiss = 0.9,
+             pcaType = pca_type, 
+             matrixType = matrix_type
+        )
+        
+        return(NULL)
+}
 
-imco(files, mask,
-     out_dir = "output/niftis/coupled/global_wcov",
-     out_name = "3234",
-     ref = 1, fwhm = 3,
-     propMiss = 0.9, pcaType = "global", matrixType = "wcov"
-)
+change_mode("global_wcov")
+change_mode("unscaled_wcor")
 
-imco(files, mask,
-     ref = 1, fwhm = 3, retimg = T, type = "pca",
-     propMiss = 0.9, pcaType = "global", matrixType = "wcov"
-)
