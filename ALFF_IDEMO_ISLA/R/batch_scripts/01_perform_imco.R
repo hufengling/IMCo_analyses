9b81956ba7be94ceae94b600af39156fdb3acbe7
hufengling <hufengling@gmail.com>
2021-09-17 15:17:26 -0400

    successfully generated barplots. working on barplots for raw data.

--- ALFF_IDEMO_ISLA/R/batch_scripts/01_perform_imco.R
+++ ALFF_IDEMO_ISLA/R/batch_scripts/01_perform_imco.R
@@ -3,14 +3,14 @@ library(stringr)
 library(magrittr)
 
 args <- commandArgs(trailingOnly = TRUE)
-files <- args[1] %>% 
-        str_split(pattern = ",") %>% 
-        unlist() %>% 
+files <- args[1] %>%
+        str_split(pattern = ",") %>%
+        unlist() %>%
         as.list()
 mask <- args[2]
 output_dir <- args[3]
 out_name <- args[4]
-fwhm <- args[5]
+fwhm <- args[5] %>% as.numeric()
 propMiss <- args[6]
 
 change_mode <- function(mode) {
@@ -24,20 +24,20 @@ change_mode <- function(mode) {
                 matrix_type <- "wcor"
         } else
                 stop("Must enter global_wcov or unscaled_wcor")
-        
+
         mode_output_dir <- file.path(output_dir, "niftis/coupled", path)
         dir.create(mode_output_dir, showWarnings = FALSE, recursive = TRUE)
-        
-        imco(files, 
+
+        imco(files,
              mask,
              out_dir = mode_output_dir,
              out_name = out_name,
              fwhm = fwhm,
              propMiss = propMiss,
-             pcaType = pca_type, 
+             pcaType = pca_type,
              matrixType = matrix_type
         )
-        
+
         return(NULL)
 }
 
