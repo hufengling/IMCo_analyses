de896293cba885ba86cc27c5ecb32f38f48faddf
hufengling <hufengling@gmail.com>
2021-10-27 17:58:28 -0400

    running with corrected BASIL and logit IMCo

--- ALFF_BASIL_IDEMO/R/batch_scripts/01_perform_imco.R
+++ ALFF_BASIL_IDEMO/R/batch_scripts/01_perform_imco.R
@@ -1,46 +1,34 @@
 library(IMCo)
 library(stringr)
-library(magrittr)
 
 args <- commandArgs(trailingOnly = TRUE)
-files <- args[1] %>%
-        str_split(pattern = ",") %>%
-        unlist() %>%
-        as.list()
+files <- as.list(unlist(str_split(args[1], pattern = ",")))
 mask <- args[2]
 output_dir <- args[3]
-out_name <- args[4]
-fwhm <- args[5] %>% as.numeric()
-prop_miss <- args[6] %>% as.numeric()
+out_name <- paste0(args[4], "_", args[5])
+fwhm <- as.numeric(args[5])
+prop_miss <- as.numeric(args[6])
+
 
 change_mode <- function(mode) {
-        if (mode == "global_wcov") {
-                path <- "global_wcov"
-                pca_type <- "global"
-                matrix_type <- "wcov"
-        } else if (mode == "unscaled_wcor") {
-                path <- "unscaled_wcor"
-                pca_type <- "unscaled"
-                matrix_type <- "wcor"
-        } else
-                stop("Must enter global_wcov or unscaled_wcor")
+  if (mode != "global_wcov" & mode != "unscaled_wcor") {
+    stop("Must enter global_wcov or unscaled_wcor")
+  }
 
-        mode_output_dir <- file.path(output_dir, "niftis/coupled", path)
-        dir.create(mode_output_dir, showWarnings = FALSE, recursive = TRUE)
+  mode_output_dir <- file.path(output_dir, "niftis/coupled", mode)
+  dir.create(mode_output_dir, showWarnings = FALSE, recursive = TRUE)
 
-        imco(files,
-             mask,
-             out_dir = mode_output_dir,
-             out_name = out_name,
-             fwhm = fwhm,
-             prop_miss = prop_miss,
-             pca_type = pca_type,
-             matrix_type = matrix_type
-        )
+  imco(files = files,
+       brain_mask = mask,
+       out_dir = mode_output_dir,
+       out_name = out_name,
+       fwhm = fwhm,
+       prop_miss = prop_miss,
+       pca_type = mode
+  )
 
-        return(NULL)
+  return(NULL)
 }
 
 change_mode("global_wcov")
 change_mode("unscaled_wcor")
-
---
+++ input/basil_rename.R
@@ -0,0 +1,19 @@
+library(tidyverse)
+library(extrantsr)
+library(ANTsRCore)
+
+orig_basil_path <- list.files("niftis/basilPNC", full.name = TRUE)
+orig_basil_name <- list.files("niftis/basilPNC")
+basil_ids <- read_csv("bblid_scanid_sub.csv", col_names = FALSE)
+new_dir <- "niftis/basil/"
+
+for (i in 1:length(orig_basil_path)) {
+  basil_image <- check_ants(orig_basil_path[i])
+  id_num <- ((orig_basil_name[i] %>% str_split("_"))[[1]][1] %>% str_split("-"))[[1]][2]
+  row_num <- which(basil_ids[, 3] == id_num)
+  scan_id <- basil_ids[row_num, 2] %>% as.numeric()
+  
+  antsImageWrite(basil_image, paste0(new_dir, scan_id, "_basil.nii.gz"))
+  
+  cat(paste("Wrote:", scan_id, "\n"))
+}
