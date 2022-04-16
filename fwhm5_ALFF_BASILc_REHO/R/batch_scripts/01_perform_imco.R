11c082e8ec334f01ccbaaf68e8e5b3e1d47806bb
hufengling <hufengling@gmail.com>
2022-04-16 13:19:06 -0400

    adding figures from simulation

---
+++ fwhm5_ALFF_BASILc_REHO/R/batch_scripts/01_perform_imco.R
@@ -0,0 +1,34 @@
+library(pIMCo)
+library(stringr)
+
+args <- commandArgs(trailingOnly = TRUE)
+files <- as.list(unlist(str_split(args[1], pattern = ",")))
+mask <- args[2]
+output_dir <- args[3]
+out_name <- paste0(args[4], "_", args[5])
+fwhm <- as.numeric(args[5])
+prop_miss <- as.numeric(args[6])
+
+
+change_mode <- function(mode) {
+        if (mode != "global_wcov" & mode != "unscaled_wcor") {
+                stop("Must enter global_wcov or unscaled_wcor")
+        }
+
+        mode_output_dir <- file.path(output_dir, "niftis/coupled", mode)
+        dir.create(mode_output_dir, showWarnings = FALSE, recursive = TRUE)
+
+        imco(files = files,
+             brain_mask = mask,
+             out_dir = mode_output_dir,
+             out_name = out_name,
+             fwhm = fwhm,
+             prop_miss = prop_miss,
+             pca_type = mode
+        )
+
+        return(NULL)
+}
+
+#change_mode("global_wcov")
+change_mode("unscaled_wcor")
